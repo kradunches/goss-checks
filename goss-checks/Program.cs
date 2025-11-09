97c213fb4e296facff5242d07bc646926056eb19
kradunches <sasha10131310@gmail.com>
2025-11-09 21:13:02 +0300

    week 6 lms

--- goss-checks/Program.cs
+++ goss-checks/Program.cs
@@ -1,4 +1,8 @@
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
+using System.Security.Cryptography;
+
+const string LOGIN = "d5e5c122-0957-4501-971a-e81248c8522c";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,7 +25,7 @@ app.UseCors(AppAllowSpecificOrigins);
 
 app.Use(async (context, next) =>
 {
-    context.Response.Headers["X-Author"] = "d5e5c122-0957-4501-971a-e81248c8522c";
+    context.Response.Headers["X-Author"] = LOGIN;
     context.Response.Headers["Access-Control-Allow-Origin"] = "*";
     context.Response.Headers["Content-Type"] = "text/plain; charset=UTF-8";
     
@@ -34,7 +38,7 @@ app.Use(async (context, next) =>
 
 app.MapGet("/", () =>
 {
-    return "d5e5c122-0957-4501-971a-e81248c8522c";
+    return LOGIN;
 });
 
 app.MapGet("/promise", () =>
@@ -73,7 +77,7 @@ app.MapGet("/fetch", () =>
 
 app.MapGet("/login", () =>
 {
-    return "d5e5c122-0957-4501-971a-e81248c8522c";
+    return LOGIN;
 });
 
 app.MapGet("/sample", () =>
@@ -89,10 +93,57 @@ app.Map("/result4", async (HttpContext context) =>
     var body = await new StreamReader(context.Request.Body).ReadToEndAsync();
     return new Dictionary<string, object>
     {
-        ["message"] = "d5e5c122-0957-4501-971a-e81248c8522c",
+        ["message"] = LOGIN,
         ["x-result"] = xTest,
         ["x-body"] = body
     };
 });
+
+app.MapGet("/hour", () =>
+{
+    return DateTime.Now.Hour;
+});
+
+app.MapGet("/code", () =>
+{
+    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Program.cs");
+    var sourceCode = File.ReadAllText(filePath);
+    return Results.Content(sourceCode, "text/plain", Encoding.UTF8, 200);
+});
+
+app.MapGet("/sha/{input}", (string input) =>
+{
+    using var sha1 = SHA1.Create();
+    var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+    return BitConverter.ToString(hash).Replace("-", "").ToLower();
+});
+
+app.Map("/req", async (HttpContext context) =>
+{
+    string addr = null;
+
+    if (context.Request.Method is "GET")
+        addr = context.Request.Query["addr"];
+    else if (context.Request.Method is "POST")
+    {
+        var form = await context.Request.ReadFormAsync();
+        addr = form["addr"];
+    }
+
+    try
+    {
+        using var client = new HttpClient();
+        var content = await client.GetStringAsync(addr);
+
+        return Results.Content(content, "text/plain", Encoding.UTF8, 200);
+    }
+    catch (Exception ex)
+    {
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapFallback(() => LOGIN);
 
 app.Run();
